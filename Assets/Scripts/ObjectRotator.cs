1764b394cbfd846756287150743513d01720922e
Igor Józefowicz <igorjozefowicz@gmail.com>
2025-05-31 01:31:10 +0200

    develop lesson 4

---
+++ Assets/Scripts/ObjectRotator.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+public class ObjectRotator : MonoBehaviour
+{
+    public float rotationSpeed = 2f;
+    public void Start()
+    {
+        //Losujemy prêdkoœæ obrotu z zakresu 50% do 150% pierwotnej
+        rotationSpeed = Random.Range(0.5f * rotationSpeed, 1.5f * rotationSpeed);
+    }
+    public void FixedUpdate()
+    {
+        //Obracamy w osi Z (do ekranu)
+        transform.Rotate(new Vector3(0f, 0f, rotationSpeed));
+    }
+}
